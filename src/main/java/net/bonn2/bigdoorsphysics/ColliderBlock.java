bbc9929b7d26452ff3cea805e1f127570a5e2b15
Brett Bonifas <bonifas.fetzer@gmail.com>
2022-12-23 16:40:51 -0500

    Add player collision detection

--- src/main/java/net/bonn2/bigdoorsphysics/ColliderBlock.java
+++ src/main/java/net/bonn2/bigdoorsphysics/ColliderBlock.java
@@ -2,16 +2,48 @@ package net.bonn2.bigdoorsphysics;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.util.BoundingBox;
+
+import java.util.Objects;
+
+import static net.bonn2.bigdoorsphysics.BigDoorsPhysics.PLUGIN;
 
 public class ColliderBlock {
     public final Location location;
+    public final Location direction;
 
-    public ColliderBlock(Location location) {
+    /**
+     * @param location The position to place the block
+     * @param direction A location that describes the direction that the block should try to push the player
+     *                  <br>It will attempt to push the player horizontally first, and after it pushes the player in one direction it will stop trying to move them.</br>
+     */
+    public ColliderBlock(Location location, Location direction) {
         this.location = location;
+        this.direction = direction;
     }
 
     public void place() {
         location.getWorld().setBlockData(location, Material.BARRIER.createBlockData());
+        if (Objects.equals(direction, new Location(location.getWorld(), 0, 0, 0))) return;
+        // Handle clipping players
+        for (Player player : PLUGIN.getServer().getOnlinePlayers()) {
+            if (new BoundingBox(
+                    location.getBlockX(),
+                    location.getBlockY(),
+                    location.getBlockZ(),
+                    location.getBlockX() + 1,
+                    location.getBlockY() + 1,
+                    location.getBlockZ() + 1
+            ).contains(
+                    player.getLocation().getX(),
+                    player.getLocation().getY(),
+                    player.getLocation().getZ()
+            )
+            && !location.getWorld().getType(location.add(direction)).isCollidable()) {
+                player.teleport(player.getLocation().add(direction));
+            }
+        }
     }
 
     public void remove() {
--- src/main/java/net/bonn2/bigdoorsphysics/PhysicsListener.java
+++ src/main/java/net/bonn2/bigdoorsphysics/PhysicsListener.java
@@ -1,17 +1,15 @@
 package net.bonn2.bigdoorsphysics;
 
 import com.destroystokyo.paper.event.server.ServerTickEndEvent;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.util.TriState;
 import nl.pim16aap2.bigDoors.BigDoors;
 import nl.pim16aap2.bigDoors.events.DoorEventToggleEnd;
 import nl.pim16aap2.bigDoors.events.DoorEventToggleStart;
 import nl.pim16aap2.bigDoors.moveBlocks.BlockMover;
 import nl.pim16aap2.bigDoors.util.MyBlockData;
-import org.bukkit.entity.Shulker;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
@@ -20,59 +18,75 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static net.bonn2.bigdoorsphysics.BigDoorsPhysics.PLUGIN;
-
 public class PhysicsListener implements Listener {
 
-    private Map<Long, BlockMover> blockMovers = new HashMap<>();
-    private Map<Long, List<ColliderBlock>> colliders = new HashMap<>();
+    private final Map<Long, BlockMover> BLOCK_MOVERS = new HashMap<>();
+    private final Map<Long, List<ColliderBlock>> COLLIDERS = new HashMap<>();
 
     @EventHandler
     public void onBigDoorsToggleStart(@NotNull DoorEventToggleStart startEvent) {
         if (startEvent.instantOpen()) return;
-        blockMovers.put(startEvent.getDoor().getDoorUID(), BigDoors.get().getCommander().getBlockMover(startEvent.getDoor().getDoorUID()));
+        BLOCK_MOVERS.put(startEvent.getDoor().getDoorUID(), BigDoors.get().getCommander().getBlockMover(startEvent.getDoor().getDoorUID()));
     }
 
     @EventHandler
     public void onBigDoorsToggleEnd(@NotNull DoorEventToggleEnd endEvent) {
         if (endEvent.instantOpen()) return;
-        for (ColliderBlock block : colliders.get(endEvent.getDoor().getDoorUID())) {
+        for (ColliderBlock block : COLLIDERS.get(endEvent.getDoor().getDoorUID())) {
             block.remove();
         }
-        colliders.remove(endEvent.getDoor().getDoorUID());
-        blockMovers.remove(endEvent.getDoor().getDoorUID());
-        PLUGIN.getLogger().info("b");
+        COLLIDERS.remove(endEvent.getDoor().getDoorUID());
+        BLOCK_MOVERS.remove(endEvent.getDoor().getDoorUID());
     }
 
     @EventHandler
     public void updateCollisions(ServerTickEndEvent tickEndEvent) {
-        for (long id : blockMovers.keySet()) {
+        for (long id : BLOCK_MOVERS.keySet()) {
             List<MyBlockData> doorBlocks;
             try {
                 Field SavedBlocks = BlockMover.class.getDeclaredField("savedBlocks");
                 SavedBlocks.setAccessible(true);
                 // Get private variable of MyBlockData List
-                doorBlocks = (List<MyBlockData>) SavedBlocks.get(blockMovers.get(id));
+                doorBlocks = (List<MyBlockData>) SavedBlocks.get(BLOCK_MOVERS.get(id));
                 if (doorBlocks.size() == 0) continue;
-                if (colliders.containsKey(id)) {
-                    List<ColliderBlock> oldBlocks = colliders.get(id);
+                if (COLLIDERS.containsKey(id)) {
+                    List<ColliderBlock> oldBlocks = COLLIDERS.get(id);
                     List<ColliderBlock> blocks = new ArrayList<>(doorBlocks.size());
                     for (MyBlockData doorBlock : doorBlocks) {
-                        blocks.add(new ColliderBlock(doorBlock.getFBlock().getLocation().toCenterLocation()));
-                        colliders.put(id, blocks);
-                        PLUGIN.getLogger().info("A");
+                        Location direction = new Location(doorBlock.getFBlock().getLocation().getWorld(), 0, 0, 0);
+                        Vector velocity = doorBlock.getFBlock().getVelocity();
+                        if (velocity.getX() > 0)
+                            direction = direction.add(1,0,0);
+                        if (velocity.getX() < 0)
+                            direction = direction.add(-1, 0, 0);
+                        if (velocity.getY() > 0)
+                            direction = direction.add(0, 1, 0);
+                        if (velocity.getY() < 0)
+                            direction = direction.add(0, -1, 0);
+                        if (velocity.getZ() > 0)
+                            direction = direction.add(0, 0, 1);
+                        if (velocity.getZ() < 0)
+                            direction = direction.add(0, 0, -1);
+                        blocks.add(new ColliderBlock(
+                                    doorBlock.getFBlock().getLocation().add(0, 0.1, 0).toCenterLocation(),
+                                    direction
+                                ));
+                        COLLIDERS.put(id, blocks);
                     }
                     for (ColliderBlock oldBlock : oldBlocks)
                         oldBlock.remove();
-                    colliders.get(id).forEach(ColliderBlock::place);
+                    COLLIDERS.get(id).forEach(ColliderBlock::place);
                 }
                 else {
                     List<ColliderBlock> blocks = new ArrayList<>(doorBlocks.size());
                     for (MyBlockData doorBlock : doorBlocks) {
-                        blocks.add(new ColliderBlock(doorBlock.getFBlock().getLocation().toBlockLocation()));
-                        colliders.put(id, blocks);
+                        blocks.add(new ColliderBlock(
+                                doorBlock.getFBlock().getLocation().add(0, 0.1, 0).toBlockLocation(),
+                                new Location(doorBlock.getFBlock().getLocation().getWorld(), 0, 0, 0)
+                        ));
+                        COLLIDERS.put(id, blocks);
                     }
-                    colliders.get(id).forEach(ColliderBlock::place);
+                    COLLIDERS.get(id).forEach(ColliderBlock::place);
                 }
             } catch (NoSuchFieldException | IllegalAccessException e) {
                 e.printStackTrace();
