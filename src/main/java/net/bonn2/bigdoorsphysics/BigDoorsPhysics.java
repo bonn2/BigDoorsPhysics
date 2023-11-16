29b352c58086bb53cb4bd30d8e864c4a1d0e1b4e
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2023-11-16 01:19:24 -0500

    Implement ChunkLoadListener

--- src/main/java/net/bonn2/bigdoorsphysics/BigDoorsPhysics.java
+++ src/main/java/net/bonn2/bigdoorsphysics/BigDoorsPhysics.java
@@ -3,10 +3,7 @@ package net.bonn2.bigdoorsphysics;
 import net.bonn2.bigdoorsphysics.barriermethod.BarrierListener;
 import net.bonn2.bigdoorsphysics.barriermethod.ColliderBlock;
 import net.bonn2.bigdoorsphysics.bstats.Metrics;
-import net.bonn2.bigdoorsphysics.shulkermethod.ColliderShulker;
-import net.bonn2.bigdoorsphysics.shulkermethod.CommandListener;
-import net.bonn2.bigdoorsphysics.shulkermethod.ShulkerListener;
-import net.bonn2.bigdoorsphysics.shulkermethod.ShulkerPacketEditor;
+import net.bonn2.bigdoorsphysics.shulkermethod.*;
 import net.bonn2.bigdoorsphysics.util.CollisionMethod;
 import net.bonn2.bigdoorsphysics.util.Config;
 import net.bonn2.bigdoorsphysics.util.ModrinthUpdateChecker;
@@ -22,6 +19,7 @@ public final class BigDoorsPhysics extends JavaPlugin {
     public static VersionUtil VERSION_UTIL;
     public static BarrierListener BARRIER_LISTENER;
     public static ShulkerListener SHULKER_LISTENER;
+    public static ChunkLoadListener CHUNK_LOAD_LISTENER;
 
     @Override
     public void onEnable() {
@@ -66,6 +64,11 @@ public final class BigDoorsPhysics extends JavaPlugin {
 
             // Start collisions updater
             getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> SHULKER_LISTENER.updateCollisions(), 1, 1);
+
+            if (Config.removeShulkersOnChunkLoad()) {
+                CHUNK_LOAD_LISTENER = new ChunkLoadListener();
+                getServer().getPluginManager().registerEvents(CHUNK_LOAD_LISTENER, this);
+            }
         }
 
         metrics.addCustomChart(new Metrics.SimplePie("door_method", () -> Config.getCollisionMethod().getOrDefault(DoorType.DOOR, CollisionMethod.NONE).name()));
---
+++ src/main/java/net/bonn2/bigdoorsphysics/shulkermethod/ChunkLoadListener.java
@@ -0,0 +1,36 @@
+package net.bonn2.bigdoorsphysics.shulkermethod;
+
+import net.bonn2.bigdoorsphysics.BigDoorsPhysics;
+import net.bonn2.bigdoorsphysics.util.Config;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Shulker;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class ChunkLoadListener implements Listener {
+
+    @EventHandler
+    public void onChunkLoad(@NotNull ChunkLoadEvent event) {
+        // Never run this method unless config is enabled
+        if (!Config.removeShulkersOnChunkLoad()) return;
+        // Remove all physics entities inside newly loaded chunk
+        int count = 0;
+        for (Entity entity : event.getChunk().getEntities()) {
+            if (entity instanceof Shulker && Objects.equals(entity.getCustomName(), "BigDoorsPhysicsS")) {
+                entity.remove();
+                count++;
+            }
+            if (entity instanceof ArmorStand && Objects.equals(entity.getCustomName(), "BigDoorsPhysicsAS")) {
+                entity.remove();
+                count++;
+            }
+        }
+        if (count > 0)
+            BigDoorsPhysics.PLUGIN.getLogger().info("Cleaned up " + count + " physics entities in chunk " + event.getChunk().getX() + " " + event.getChunk().getZ());
+    }
+}
