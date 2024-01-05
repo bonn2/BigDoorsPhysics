1079db5968e409702d8b9a9dc6a45de46e574102
Brett Bonifas <bonifas.fetzer@gmail.com>
2024-01-05 11:03:49 -0500

    Load correct VersionUtil

--- versions/v1_20_2/src/main/java/net/bonn2/bigdoorsphysics/versions/v1_20_2/VersionUtil_v1_20_2.java
+++ versions/v1_20_2/src/main/java/net/bonn2/bigdoorsphysics/versions/v1_20_2/VersionUtil_v1_20_2.java
@@ -2,6 +2,8 @@ package net.bonn2.bigdoorsphysics.versions.v1_20_2;
 
 import io.papermc.paper.entity.TeleportFlag;
 import net.bonn2.bigdoorsphysics.versions.VersionUtil;
+import org.apache.maven.artifact.versioning.ComparableVersion;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -10,6 +12,9 @@ public class VersionUtil_v1_20_2 implements VersionUtil {
 
     @Override
     public boolean test() {
+        // Check if version is before 1.20.2
+        if (new ComparableVersion(Bukkit.getMinecraftVersion()).compareTo(new ComparableVersion("1.20.2")) < 0) return false;
+        // Check if server supports new teleport system
         try {
             Class.forName("io.papermc.paper.entity.TeleportFlag");
         } catch (ClassNotFoundException e) {
